f47caa153c6168cb1b6bab1ba07a23fb96f82550
sanjeev analyst <sanjeevkumar2062cs@gmail.com>
2026-05-28 17:38:09 +0530

    create superuser improvement

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
-fi
+# if [[ $CREATE_SUPERUSER ]]; then
+#     python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+# fi
